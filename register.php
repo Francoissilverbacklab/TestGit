6885c86684d0ff53d2ff2ade2d2adccd87c55e10
Francoissilverbacklab <francois@silverbacklab.com>
2019-04-18 08:00:07 +0200

    PHP OOP Login/Register System: CSRF Protection (Part 12/23)

--- register.php
+++ register.php
@@ -2,38 +2,44 @@
 
 require_once 'core/init.php';
 
-if(Input::exists()) {
-	$validate = new Validate();
-	$validation = $validate->check($_POST, array(
-		'username' =>  array(
-			'required' => true,
-			'min' => 2,
-			'max' => 20,
-			'unique' => 'users'
-		),
-		'password' =>  array(
-			'required' => true,
-			'min' => 6
-		),
-		'password_again' =>  array(
-			'required' => true,
-			'matches' => 'password'
-		),
-		'name' =>  array(
-			'required' => true,
-			'min' => 2,
-			'max' => 50
-		)
-	));
 
-	if($validation->passed()) {
-		echo 'Passed';
-	} else {
-		foreach ($validation->errors() as $error) {
-			echo $error, '<br>';
+if(Input::exists()) {
+	if(Token::check(Input::get('token'))) {
+
+		$validate = new Validate();
+		$validation = $validate->check($_POST, array(
+			'username' =>  array(
+				'required' => true,
+				'min' => 2,
+				'max' => 20,
+				'unique' => 'users'
+			),
+			'password' =>  array(
+				'required' => true,
+				'min' => 6
+			),
+			'password_again' =>  array(
+				'required' => true,
+				'matches' => 'password'
+			),
+			'name' =>  array(
+				'required' => true,
+				'min' => 2,
+				'max' => 50
+			)
+		));
+
+		if($validation->passed()) {
+			echo 'Passed';
+		} else {
+			foreach ($validation->errors() as $error) {
+				echo $error, '<br>';
+			}
 		}
+		
 	}
 }
+
 
 ?>
 
@@ -57,6 +63,7 @@ if(Input::exists()) {
 		<label for="name">Your name</label>
 		<input type="text" name="name" id="name" value="<?php echo escape(Input::get('name')); ?>">
 	</div>
-
+	
+	<input type="hidden" name="token" value="<?php echo Token::generate(); ?>" value="Register">
 	<input type="submit" value="Register">
 </form>
